097ffcfb9107b28dd22c2779e3bc2d4c81e8cd84
Jerome Gacoscosim <40538528+jeromeski@users.noreply.github.com>
2021-05-22 15:57:41 +0800

    Updated to tut solution

--- src/App.js
+++ src/App.js
@@ -2,31 +2,51 @@ import { Fragment, useEffect, useRef, useState } from "react";
 import "./styles.css";
 
 function ClickGame() {
-  const [clickCount, setClickCount] = useState(0);
-  const [countDown, setCountDown] = useState(10);
+  const [count, setCount] = useState(0);
+  const [timeLeft, setTimeLeft] = useState(10);
 
-  const countDownRef = useRef(null);
-  const timeoutRef = useRef(null)
 
-  const handleClick = () => {
-    setClickCount(c => c + 1)
-  }
+  const id = useRef()
+
+  const clear = () => clearInterval(id.current)
 
   useEffect(() => {
-    countDownRef.current = setInterval(() => {
-      setCountDown(prev => prev - 1)
-    },1000)
+    id.current = setInterval(() => {
+      setTimeLeft(t => t - 1)
+    }, 1000)
 
-    timeoutRef.current = setTimeout(() => {
-      clearInterval(countDownRef.current)
-    },10000)
+    return clear
+
   },[])
+
+  useEffect(() => {
+    if(timeLeft === 0) {
+      clear()
+    }
+  },[timeLeft])
+
+  // const countDownRef = useRef(null);
+  // const timeoutRef = useRef(null)
+
+  // const handleClick = () => {
+  //   setClickCount(c => c + 1)
+  // }
+
+  // useEffect(() => {
+  //   countDownRef.current = setInterval(() => {
+  //     setCountDown(prev => prev - 1)
+  //   },1000)
+
+  //   timeoutRef.current = setTimeout(() => {
+  //     clearInterval(countDownRef.current)
+  //   },10000)
+  // },[])
 
   return (
     <Fragment>
-      <h1>{`Clicks : ${clickCount}`}</h1>
-      <h1>{`Timer : ${countDown}`}</h1>
-      {countDown === 0 ? '' : <button onClick={handleClick}>Click</button>}
+      <h1>{`Clicks : ${count}`}</h1>
+      <h1>{`Timer : ${timeLeft}`}</h1>
+      {timeLeft === 0 ? null : <button onClick={() => setCount(c => c + 1)}>Click</button>}
       
     </Fragment>
   )
